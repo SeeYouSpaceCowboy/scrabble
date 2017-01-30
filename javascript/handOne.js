cdb94f66ebe59eb6892daf016b2e528180d24c1b
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-01-30 15:55:01 -0500

    Bug fixes

--- javascript/handOne.js
+++ javascript/handOne.js
@@ -10,10 +10,7 @@ class HandOne{
     let missing = this.getHand()
 
     for(var i = 0; i < missing; i++){
-      let x = $('div.one').append(`<div class="handSquare">
-      <h5 class="align-middle">${this.handTiles[i][0]}</h5>
-      <p class="points">${this.handTiles[i][1]}</p>
-      </div>`)
+      let x = $('div.one').append(`<div class="handSquare"><h5 class="align-middle">${this.handTiles[i][0]}</h5><p class="points">${this.handTiles[i][1]}</p></div>`)
     }
 
     $('#score_one').text(this.score)
